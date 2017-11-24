d1ee2649a89d5aea3fe4e5d9035b5f0a1787dea1
bluepeartree <sherrie.sebastian@gmail.com>
2017-11-24 00:45:06 -0500

    add functionality added

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,19 +1,35 @@
 class EntriesController < ApplicationController
 
 def index
+  @entries=Entry.all
   render :index
 end
 
 def show
+  @entry = Entry.find(params[:id])
   render :show
 end
 
 def new
+  @entry = Entry.new
   render :new
 end
 
 def create
-  redirect_to entries_url
+  @entry = Entry.new
+
+  @entry.word = params[:entry][:word]
+  @entry.language = params[:entry][:language]
+  @entry.definition = params[:entry][:definition]
+
+
+  if @entry.save
+    # if the picture gets saved, generate a get request to "/pictures" (the index)
+    redirect_to entries_url
+  else
+    # otherwise render new.html.erb
+    render :new
+  end
 end
 
 def edit
